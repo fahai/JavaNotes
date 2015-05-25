6b736d5426e5c8eff0dda41716b83440f712b9c4
fahai <lihairong.cn@gmail.com>
2015-05-25 14:14:49 +0800

    add ignore files

---
+++ src/org/fahai/app/tmp/TestInetAdress.java
@@ -0,0 +1,31 @@
+/* 
+ * Copyright (c) 2015, S.F. Express Inc. All rights reserved.
+ */
+package org.fahai.app.tmp;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * 描述：
+ * 
+ * @author 449631
+ * @since 1.0
+ */
+public class TestInetAdress {
+
+	/**
+	 * @param args
+	 * void
+	 */
+	public static void main(String[] args) {
+		InetAddress test;
+		try {
+			test = InetAddress.getLocalHost();
+			System.out.println(test.getHostAddress());
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
