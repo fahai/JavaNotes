518a328c26ef19a4f2d8f42a8340e6c3f5adf257
fahai <lihairong.cn@gmail.com>
2015-06-01 23:50:45 +0800

    jikexueyuan thread and singleton

---
+++ src/org/fahai/jikexueyuan/pattern/Singleton.java
@@ -0,0 +1,11 @@
+package org.fahai.jikexueyuan.pattern;
+
+/**
+ * 单例模式的实现原理、类图
+ * 多线程优化方案
+ * @author fahai
+ *
+ */
+public class Singleton {
+
+}
---
+++ src/org/fahai/jikexueyuan/thread/Ticket.java
@@ -0,0 +1,16 @@
+package org.fahai.jikexueyuan.thread;
+
+/**
+ * 线程的实现方法：继承Thread类 or 实现Runnable接口
+ * 线程的常用方法：getName(),currentThread(),isAlive(),sleep(), yield()
+ * 优先级
+ * 同步与死锁：实现简单的售票程序(资源共享问题)，理解死锁
+ * 生命周期：创建， 就绪， 阻塞， 运行， 停止
+ * @author fahai
+ *
+ */
+public class Ticket {
+
+	
+	
+}
